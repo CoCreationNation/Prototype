088c06a852ac00adf92b4f3a74a7e3bd835b234c
Ed Nunes <ed.a.nunes@gmail.com>
2021-08-29 10:19:54 -0700

    Add base template

--- services/web/project/views.py
+++ services/web/project/views.py
@@ -1,8 +1,10 @@
 """
 This file contains all the routes for the Flask app.
 """
+from flask import render_template
+
 from project import app
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('home.html')
