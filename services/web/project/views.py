654c34d901be272b1525305c02f9cb646db438de
Vaidehee Bhise <vaideheebhise@Vaidehees-MacBook-Pro.local>
2021-09-03 16:30:19 -0700

    address pr comments

--- services/web/project/views.py
+++ services/web/project/views.py
@@ -1,10 +1,34 @@
 """
 This file contains all the routes for the Flask app.
 """
+from Prototype.services.web.project import forms
 from flask import render_template
 
 from project import app
+from project import forms
 
-@app.route('/')
-def index():
-    return render_template('home.html')
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = forms.LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template('login.html', form=login_form)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    registration_form = forms.RegistrationForm()
+    # if registration_form.validate_on_submit():
+    return render_template('register.html', form=registration_form)
+
+# while it is usually better to do all imports at the top of the file, importing here mid-file is a best practice for Flask
+from project import models  
+from project import views
