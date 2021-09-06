d4332c25933ecfe05837e91ae50b4307b3b2da7c
Vaidehee Bhise <vaideheebhise@Vaidehees-MacBook-Pro.local>
2021-09-06 10:12:47 -0700

    Adressing the PR comments

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -12,3 +12,7 @@ app.config.from_object("project.config.Config")
 app.secret_key = "any-string"
 Bootstrap(app)
 db = SQLAlchemy(app)
+
+# while it is usually better to do all imports at the top of the file, importing here mid-file is a best practice for Flask
+from project import models  
+from project import views
