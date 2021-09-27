60b446a6d7cff187ddd5c95e22eb768e00143d7e
Vaidehee Bhise <vaideheebhise@Vaidehees-MacBook-Pro.local>
2021-09-27 15:50:30 -0700

    add eventforms

--- services/web/project/forms.py
+++ services/web/project/forms.py
@@ -3,8 +3,9 @@
 This file is used to initiate all forms
 """
 
+#from Prototype.services.web.project.models import Event
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, PasswordField, SubmitField
+from wtforms import StringField, TextAreaField, PasswordField, SubmitField, IntegerField, BooleanField
 from wtforms.fields.html5 import DateTimeLocalField
 from wtforms.validators import DataRequired, Email, Length
 
@@ -27,3 +28,18 @@ class RegistrationForm(FlaskForm):
     password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     email = StringField(label='Email', validators=[DataRequired(), Email()])
     zip_code = StringField(label='Zip Code')
+
+class UserEventForm(FlaskForm):
+    id = IntegerField('ID')
+    event_id = IntegerField('Event ID', validators=[DataRequired()])
+    attendee_id = IntegerField('Attendeee ID', validators=[DataRequired()])
+    attended_at= BooleanField('Attended')
+    zipcode=IntegerField('Zipcode', validators=[DataRequired()])
+
+class EventAdminForm(FlaskForm):
+    id = IntegerField('ID')
+    user_id = IntegerField('User ID', validators=[DataRequired()])
+    event_id = IntegerField('Event ID', validators=[DataRequired()])
+    #rsvp_at = StringField('Email for RSVP',validators=[DataRequired()]) 
+    admin_level = StringField('Admin Level')
+
