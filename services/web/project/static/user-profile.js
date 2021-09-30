e89e73ab8987e4f244412f148afe2722c94832a8
abercilla <annebercilla@gmail.com>
2021-09-30 12:05:41 -0700

    wip - add user-profile.js to /static

---
+++ services/web/project/static/user-profile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// UPLOAD PROFILE PICTURES TO S3
+
+// alert("create-event.js is connected!");
+
+//when you click "Upload Image"
+var loadFile = function(event) {
+    //grab 
+    var image = document.getElementById('output');
+    //create a URL out of the uploaded image file
+    console.log(image);
+    
+    image.src = URL.createObjectURL(event.target.files[0]);
+    
+};
+ 
