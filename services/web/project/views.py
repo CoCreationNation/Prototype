acb22fb98b43e98e28b2f965a3d274aff1014af2
abercilla <annebercilla@gmail.com>
2021-10-04 17:02:55 -0700

    wip, modified edit-profile in views.py

--- services/web/project/views.py
+++ services/web/project/views.py
@@ -128,29 +128,29 @@ def show_profile(user_id):
     
     return render_template("user-profile.html", user=user, events=attended_events, user_in_session=user_in_session)
 
-@app.route('/edit-profile/<user_id>', methods=["GET", "POST"])
-@login_required
-def edit_profile(user_id): 
-    """Gather and/or save edited info to signed in user"""
+# @app.route('/edit-profile/<user_id>', methods=["GET", "POST"])
+# @login_required
+# def edit_profile(user_id): 
+#     """Gather and/or save edited info to signed in user"""
 
-    user = helpers.get_user_info(user_id)
-    attended_events = helpers.get_user_events(user_id)
-    user_in_session = current_user
+#     user = helpers.get_user_info(user_id)
+#     attended_events = helpers.get_user_events(user_id)
+#     user_in_session = current_user
 
-    form = forms.RegistrationForm() #take in updated info using registration form
-    first_name = form.first_name.data 
-    last_name = form.last_name.data
-    # username = form.username.data
-    # zip_code = form.zip_code.data
+#     form = forms.RegistrationForm() #take in updated info using registration form
+#     first_name = form.first_name.data 
+#     last_name = form.last_name.data
+#     # username = form.username.data
+#     # zip_code = form.zip_code.data
 
 
 
-    #for if "Update Profile" button is clicked
-    if request.method == "POST": 
-        user = helpers.update_user_details(first_name, last_name) #TODO: add helper func
+#     #for if "Update Profile" button is clicked
+#     if request.method == "POST": 
+#         user = helpers.update_user_details(first_name, last_name) #TODO: add helper func
     
-    #TODO: check duplicate username if we update that
-    return render_template("user-profile.html", user=user, events=attended_events, user_in_session=user_in_session)
+#     #TODO: check duplicate username if we update that
+#     return render_template("user-profile.html", user=user, events=attended_events, user_in_session=user_in_session)
     
 
 
